fdb2dae5cb2f40765870b0309e218eb63ae220fa
Estefan Hu <estefanhu074@gmail.com>
2019-12-08 21:14:10 -0800

    Installed base routing for dashboard app

--- StudyDubs/settings.py
+++ StudyDubs/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dashboard',
     'studygroups.apps.StudygroupsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- StudyDubs/urls.py
+++ StudyDubs/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include("studygroups.urls")),
+    path('dashboard', include("dashboard.urls")),
     path('admin/', admin.site.urls),
 ]
