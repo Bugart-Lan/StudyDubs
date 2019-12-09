285a969941f880a1c91b8a14b4734aafb294880a
Estefan Hu <estefanhu074@gmail.com>
2019-12-08 23:08:01 -0800

    started using named routes

--- login/urls.py
+++ login/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
   path("", views.getLogin, name = "login"),
   path("loginUser/", views.loginUser, name = "loginUser"),
   path("register/", views.getRegister, name = "register"),
+  path("registerUser", views.registerUser, name = "registerUser"),
   path("success/", views.success, name = "success")
 ]
